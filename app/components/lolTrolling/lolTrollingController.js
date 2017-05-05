86a3d146dd93c4cbc616d0df7d934516d28260ac
Jallyy <imortal78>
2017-05-05 12:32:11 +0200

    maj

--- app/components/lolTrolling/lolTrollingController.js
+++ app/components/lolTrolling/lolTrollingController.js
@@ -3,12 +3,33 @@ var ctrlApp = angular.module('ctrlApp');
 ctrlApp.controller('lolTrollingController', function($scope, $rootScope, $http, $location) {
 
 	//déclaration des variables et scopes
-
+    var data = {};
+    $scope.nbJoueur;
+    $scope.champRandom;
+    $scope.equipRandom;
+    $scope.sortInvoRandom;
+    $scope.roleRandom;
 
 	$scope.onCreate = function () {
-
+        $scope.initVariables();
 	}
 
+    $scope.initVariables = function () {
+        $scope.nbJoueur = true;
+        $scope.champRandom = true;
+        $scope.equipRandom = true;
+        $scope.sortInvoRandom = true;
+        $scope.roleRandom = true;
+    }
+
+    $scope.getSelections = function () {
+        data = {nbJoueur : $scope.nbJoueur, 
+                champion: $scope.champRandom,
+                equipement: $scope.equipRandom,
+                sortInvo: $scope.sortInvoRandom,
+                role: $scope.roleRandom};
+        console.log(data);
+    }
 
     //lancement des controles initiales
     $scope.onCreate();
